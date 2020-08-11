84512e5e3a1cfa602dde23ad43348c6ae083bdd5
Rory Jarrard <rory@crucibledevlabs.com>
2020-08-11 14:19:46 -0600

    article show method and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root "pages#home"
 
-  get 'about', to: 'pages#about'
+  get "about", to: "pages#about"
+
+  resources :articles, only: [:show]
 end
